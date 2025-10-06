ad00251c736e1b0c30faad3d6fab2a6181a8470b
Abdelaziz Hamdy <abdelazizhamdy74@gmail.com>
2025-10-06 14:16:57 +0300

    Level One

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: '/about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((c) => c.AboutComponent),
+  },
+  {
+    path: '/client',
+    loadComponent: () =>
+      import('./pages/client/client.component').then((c) => c.ClientComponent),
+  },
+  {
+    path: '/faq',
+    loadComponent: () =>
+      import('./pages/faq/faq.component').then((c) => c.FaqComponent),
+  },
+  {
+    path: '/services',
+    loadComponent: () =>
+      import('./pages/services/services.component').then(
+        (c) => c.ServicesComponent
+      ),
+  },
+  {
+    path: '/service/:id',
+    loadComponent: () =>
+      import('./pages/service/service.component').then(
+        (c) => c.ServiceComponent
+      ),
+  },
+  {
+    path: '/login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: '/register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
+];
